2d6cb266639d5d28afc0789c5d334f326d239061
IgorIvanov <igor.ivanov@alseda.by>
2022-11-09 15:24:24 +0300

    fixed divider in tabs

--- app/src/main/java/com/example/expenseobserver/components/Tabs.kt
+++ app/src/main/java/com/example/expenseobserver/components/Tabs.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Tab
@@ -31,12 +32,11 @@ fun ThreeTabsLay(tabList: List<Any>, currentState: MutableState<Int>) {
         modifier = Modifier
             .fillMaxWidth()
             .requiredHeight(36.dp)
-            .clip(RoundedCornerShape(10.dp)),
+            .clip(RoundedCornerShape(10.dp))
+            .border(BorderStroke(1.dp, buttonColor), RoundedCornerShape(10.dp)),
         selectedTabIndex = currentState.value,
         backgroundColor = buttonColor,
-        indicator = { tabPositions ->
-
-        }
+        indicator = { }
     ) {
         tabList.forEachIndexed { index, transactionTabItem ->
             val titleText = when (transactionTabItem) {
@@ -46,15 +46,7 @@ fun ThreeTabsLay(tabList: List<Any>, currentState: MutableState<Int>) {
             }
             Tab(
                 modifier = Modifier
-                    .border(
-                        BorderStroke(1.dp, buttonColor),
-                        when (index) {
-                            0 -> RoundedCornerShape(10.dp, 0.dp, 0.dp, 10.dp)
-                            1 -> RoundedCornerShape(0.dp)
-                            2 -> RoundedCornerShape(0.dp, 10.dp, 10.dp, 0.dp)
-                            else -> RoundedCornerShape(0.dp)
-                        }
-                    )
+                    .padding(if (index == 1) 1.dp else 0.dp)
                     .background(
                         if (currentState.value == index) buttonColor else backgroundColor,
                         RoundedCornerShape(if (currentState.value == index) 10.dp else 0.dp)
