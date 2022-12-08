eaa8fb2dd727d1f0c4930ad80071d72be0c2957a
IgorIvanov <igor.ivanov@alseda.by>
2022-12-08 10:52:55 +0300

    added opensans font

--- app/src/main/java/com/example/expenseobserver/ui/theme/Type.kt
+++ app/src/main/java/com/example/expenseobserver/ui/theme/Type.kt
@@ -7,15 +7,13 @@ import androidx.compose.ui.text.font.FontWeight
 import com.example.expenseobserver.R
 
 val fonts = FontFamily(
-    Font(R.font.raleway_medium, weight = FontWeight.Medium),
-    Font(R.font.raleway_bold, weight = FontWeight.Bold),
-    Font(R.font.raleway_semibold, weight = FontWeight.SemiBold),
-    Font(R.font.raleway_extrabold, weight = FontWeight.ExtraBold),
-    Font(R.font.raleway_extralight, weight = FontWeight.ExtraLight),
-    Font(R.font.raleway_regular, weight = FontWeight.Normal),
-    Font(R.font.raleway_light, weight = FontWeight.Light),
-    Font(R.font.raleway_thin, weight = FontWeight.Thin),
+    Font(R.font.opensans_medium, weight = FontWeight.Medium),
+    Font(R.font.opensans_bold, weight = FontWeight.Bold),
+    Font(R.font.opensans_semibold, weight = FontWeight.SemiBold),
+    Font(R.font.opensans_extrabold, weight = FontWeight.ExtraBold),
+    Font(R.font.opensans_regular, weight = FontWeight.Normal),
+    Font(R.font.opensans_light, weight = FontWeight.Light),
 )
 
 // Set of Material typography styles to start with
-val Typography = Typography()
+val Typography = Typography(defaultFontFamily = fonts)
